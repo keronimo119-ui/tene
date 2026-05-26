69f2225020f8b4c0bbdd5bf11b3b286212be11b0
keronimo119-ui <keronimo119@gmail.com>
2026-05-26 14:50:41 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,19 +1,27 @@
 const CACHE_NAME = 'tene-v1';
-const urlsToCache = [
-  '/tene/tene.html',
-  '/tene/manifest.json'
+const CACHE_URLS = [
+  './tene.html',
+  './manifest.json'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(CACHE_URLS))
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(cached => cached || fetch(event.request))
   );
 });
